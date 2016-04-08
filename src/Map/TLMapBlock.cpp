0d95f6121e4563d777a4f671f5949d1add32a84d
xiaou <xiaotjyu@gmail.com>
2016-04-08 16:34:53 +0800

    创建地图的时候，默认可行走可种植

--- src/Map/TLMapBlock.cpp
+++ src/Map/TLMapBlock.cpp
@@ -562,7 +562,8 @@ void TLMapBlock::create( int nRow, int nCol, int nWidth, int nHeight )
     m_nWidth = nWidth;
     m_nHeight = nHeight;
 
-    m_vecGridStates.resize( nRow * nCol, 0 );
+    int nGridState = TL_GRID_FLAG_PLANT | TL_GRID_FLAG_PASS;
+    m_vecGridStates.resize( nRow * nCol, nGridState );
 
     // 重新生成网格
     recreateGridLine();
